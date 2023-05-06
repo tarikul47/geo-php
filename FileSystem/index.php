da5ea2b0e787b6c6e06d3e5447882d44d8f0ebe5
Tarikul Islam <tarikul47@gmail.comm>
2023-05-07 04:04:40 +0600

    updated

--- FileSystem/index.php
+++ FileSystem/index.php
@@ -1,4 +1,35 @@
 <?php
-echo "File System";
 
-// hello 
+/**
+ * Working with file system
+ * - scandir();
+ * - mkdir | rmdir 
+ * - file_exists()
+ * - filesize()
+ * - file_put_contents()
+ * - clearstatcache()
+ * - fopen() | @fopne() 
+ * - file_get_contents()
+ * - copy();
+ * - rename()
+ * - pathinfo()
+ */
+
+/**
+ * file_put_contents() 
+ * parameter = 3 (pathname, content, mode)
+ * - FILE_USE_INCLUDE_PATH 
+ * - FILE_APPEND = can added append 
+ * - LOCK_EX 
+ */
+
+/**
+ * scandir()
+ * parameter = __DIR__
+ * we get all file name in the directory 
+ * 
+$dir = scandir(__FILE__);
+echo "<pre>";
+print_r($dir);
+ */
+
