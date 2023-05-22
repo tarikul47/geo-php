2c1662697a8d80f30c8fd39c98f13184c2da9aec
Tarikul Islam <tarikul47@gmail.comm>
2023-05-22 16:49:45 +0600

    update

--- 5.Types/string.php
+++ 5.Types/string.php
@@ -2,6 +2,28 @@
 
 /**
  * Heredoc and Nowdoc string 
+ * 
+ * 1. heredoc supports variable but Nowdoc don't support 
+ * 
+ * heredoc = <<<"EOT" | <<<EOT
+ * Nowdoc = <<<'EOT'
+ * 
+ * browser can't read new line = can use nl2br()
  */
 
-echo "Added done ";
+$variable1 = "  Raju  ";
+
+$variable = <<<"EOT"
+This is a multiline string.
+It can include variables like $variable1.
+EOT;
+
+$variable2 = <<<'EOT'
+This is a nowdoc string.
+Variables like $variable1 are treated as plain text.
+EOT;
+
+
+
+
+echo nl2br($variable2);
